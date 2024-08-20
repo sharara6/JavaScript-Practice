7a6448c092bf82854667c198e39f8afba19d2d35
Faris Sharara <122405756+sharara6@users.noreply.github.com>
2024-08-20 23:32:08 +0300

    Finalised Project

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // document.getElementById("count-el").innerText = 5;
 
+let saveEl= document.getElementById("save-el");
 let count = 0;
 let countEl = document.getElementById("count-el");
 
 function increment() {
     count += 1
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
+    let countDash = count + " - ";
+    saveEl.textContent += countDash;
     count = 0;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
